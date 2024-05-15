ebdb9fabc4d13ef25331445f967f62c6afbc2a5b
Hoyoung Song <hoysong@c3r15s2.42gyeongsan.kr>
2024-05-16 07:50:30 +0900

    fix mains

--- launch.sh
+++ launch.sh
@@ -4,6 +4,7 @@
 #|   Y  (  <_> )___  |    |  | \  ___/ \___ \  |  | \  ___/|  | \/
 #|___|  /\____// ____|____|__|  \___  >____  > |__|  \___  >__|   
 #     \/       \/   /_____/         \/     \/            \/       
+
 FLAGS="-Wall -Wextra -Werror"
 CFILES="../get_next_line.c ../get_next_line_utils.c"
 LEAK=" ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)"
@@ -65,32 +66,6 @@ do
 done
 SIZE=0
 
-echo -e "\e[43;31mothers_main.c_test\e[0m"
-while [ $SIZE -le $COUNT ]
-do
-	echo -e "\e[32mSIZE=$SIZE\e[0m"
-	cc mains/others_main.c $CFILES $FLAGS -g
-	valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-	LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-	if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-		echo  No_leaks!
-	else
-		cat log_buff_file
-	fi
-	cat cases/others/* -e > origin_output
-	cat prog_output -e > prog_output_e
-	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
-	if [ ${DIFF_RESULT} -eq 0 ]; then
-		echo -e "\e[34mDiff_ok\e[0m"
-	else
-		echo -e "\e[31mDiff_KO\e[0m"
-	fi
-	rm log_buff_file prog_output origin_output prog_output_e
-	((SIZE++))
-	echo
-done
-SIZE=0
-
 echo -e "\e[43;31mnl_btw_str_main.c_test\e[0m"
 while [ $SIZE -le $COUNT ]
 do
@@ -104,6 +79,32 @@ do
 		cat log_buff_file
 	fi
 	cat cases/nl_btw_str/* -e > origin_output
+	cat prog_output -e > prog_output_e
+	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
+	if [ ${DIFF_RESULT} -eq 0 ]; then
+		echo -e "\e[34mDiff_ok\e[0m"
+	else
+		echo -e "\e[31mDiff_KO\e[0m"
+	fi
+	rm log_buff_file prog_output origin_output prog_output_e
+	((SIZE++))
+	echo
+done
+SIZE=0
+
+echo -e "\e[43;31mothers_main.c_test\e[0m"
+while [ $SIZE -le 5 ]
+do
+	echo -e "\e[32mSIZE=$SIZE\e[0m"
+	cc mains/others_main.c $CFILES $FLAGS -g
+	valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
+	LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
+	if [ "${LEAK_RESULT}" = "${LEAK}" ];then
+		echo  No_leaks!
+	else
+		cat log_buff_file
+	fi
+	cat cases/others/* -e > origin_output
 	cat prog_output -e > prog_output_e
 	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
 	if [ ${DIFF_RESULT} -eq 0 ]; then
