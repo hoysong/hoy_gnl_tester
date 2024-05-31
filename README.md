821db075cbb61f994b25b6cf91b0459ee10812a9
Hoyoung Song <hoysong@c3r15s4.42gyeongsan.kr>
2024-06-01 05:36:10 +0900

    fix loop

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 !!THIS TESTER IS FOR MANDATORY ONLY!!
 
 0. clone this tester repository in your 'get_next_line' project directory
-1. type 'bash run.sh' to run tester
+1. type 'bash run.sh'(or 'bash launch.sh) to run tester
 2. wait for compile. (tester will make result.txt)
--- launch.sh
+++ launch.sh
@@ -1,238 +1,146 @@
-#.__                       __                   __                
-#|  |__   ____ ___.__.   _/  |_  ____   _______/  |_  ___________ 
-#|  |  \ /  _ <   |  |   \   __\/ __ \ /  ___/\   __\/ __ \_  __ \
-#|   Y  (  <_> )___  |    |  | \  ___/ \___ \  |  | \  ___/|  | \/
-#|___|  /\____// ____|____|__|  \___  >____  > |__|  \___  >__|   
-#     \/       \/   /_____/         \/     \/            \/       
 
 FLAGS="-Wall -Wextra -Werror"
+VLGND_FLG="--log-file=val_log --leak-check=full --tool=memcheck"
 CFILES="../get_next_line.c ../get_next_line_utils.c"
-LEAK=" ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)"
+NO_LEAK=" ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)"
 BF="-D BUFFER_SIZE="
-SIZE=0
 COUNT=12
 
-#echo -e "\e[47;34m\e[1m"
-#echo -e "\e[47;34mBUFFER=0\e[0m"
-#
-#echo -e "\e[;32mnl_main.c_test\e[0m"
-#cc mains/nl_main.c $BF$SIZE $CFILES $FLAGS -g
-#valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-#LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-#if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-#	echo  No_leaks!
-#else
-#	cat log_buff_file
-#fi
-#cat prog_output -e > prog_output_e
-#DIFF_RESULT=`cat prog_output_e | wc -l`
-#if [ ${DIFF_RESULT} -eq 0 ]; then
-#	echo -e "\e[34mDiff_ok\e[0m"
-#else
-#	echo -e "\e[31mDiff_KO\e[0m"
-#fi
-#rm log_buff_file prog_output prog_output_e
-#
-#echo -e "\e[;32mno_nl_main.c_test\e[0m"
-#cc mains/no_nl_main.c $BF$SIZE $CFILES $FLAGS -g
-#valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-#LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-#if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-#	echo  No_leaks!
-#else
-#	cat log_buff_file
-#fi
-#cat prog_output -e > prog_output_e
-#DIFF_RESULT=`cat prog_output_e | wc -l`
-#if [ ${DIFF_RESULT} -eq 0 ]; then
-#	echo -e "\e[34mDiff_ok\e[0m"
-#else
-#	echo -e "\e[31mDiff_KO\e[0m"
-#fi
-#rm log_buff_file prog_output prog_output_e
-#
-#echo -e "\e[;32mnl_btw_str_main.c_test\e[0m"
-#cc mains/nl_btw_str_main.c $BF$SIZE $CFILES $FLAGS -g
-#valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-#LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-#if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-#	echo  No_leaks!
-#else
-#	cat log_buff_file
-#fi
-#cat prog_output -e > prog_output_e
-#DIFF_RESULT=`cat prog_output_e | wc -l`
-#if [ ${DIFF_RESULT} -eq 0 ]; then
-#	echo -e "\e[34mDiff_ok\e[0m"
-#else
-#	echo -e "\e[31mDiff_KO\e[0m"
-#fi
-#rm log_buff_file prog_output prog_output_e
-#
-#echo -e "\e[;32mothers_main.c_test\e[0m"
-#cc mains/others_main.c $BF$SIZE $CFILES $FLAGS -g
-#valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-#LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-#if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-#	echo  No_leaks!
-#else
-#	cat log_buff_file
-#fi
-#cat prog_output -e > prog_output_e
-#DIFF_RESULT=`cat prog_output_e | wc -l`
-#if [ ${DIFF_RESULT} -eq 0 ]; then
-#	echo -e "\e[34mDiff_ok\e[0m"
-#else
-#	echo -e "\e[31mDiff_KO\e[0m"
-#fi
-#rm log_buff_file prog_output prog_output_e
-#
-#echo -e "\e[;32mivld_fd\e[0m"
-#cc mains/ivld_fd.c $BF$SIZE $CFILES $FLAGS -g
-#valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-#LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-#if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-#	echo  No_leaks!
-#else
-#	cat log_buff_file
-#fi
-#cat prog_output -e > prog_output_e
-#DIFF_RESULT=`cat prog_output_e | wc -l`
-#if [ ${DIFF_RESULT} -eq 0 ]; then
-#	echo -e "\e[34mDiff_ok\e[0m"
-#else
-#	echo -e "\e[31mDiff_KO\e[0m"
-#fi
-#rm log_buff_file prog_output prog_output_e
+del_dummys() {
+	if [ $1 -eq 1 ]; then
+		rm $2
+	fi
+}
 
-SIZE=1
-echo -e "\e[47;34m\e[1m"
-echo -e "\e[47;34mnl_main.c_test\e[0m"
-while [ $SIZE -le $COUNT ]
-do
-	echo -e "\e[32mSIZE=$SIZE\e[0m"
-	cc mains/nl_main.c $CFILES $FLAGS -g
-	valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-	LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-	if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-		echo  No_leaks!
-	else
-		cat log_buff_file
-	fi
-	cat cases/nl/* -e > origin_output
-	cat prog_output -e > prog_output_e
-	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
-	if [ ${DIFF_RESULT} -eq 0 ]; then
-		echo -e "\e[34mDiff_ok\e[0m"
-	else
-		echo -e "\e[31mDiff_KO\e[0m"
-	fi
-	rm log_buff_file prog_output origin_output prog_output_e
-	((SIZE++))
-done
-SIZE=1
+tester_init() {
+	ORIGIN=`ls | grep origin_output | wc -l`
+	del_dummys ${ORIGIN} origin_output
+	PROG=`ls | grep prog_output | wc -l`
+	del_dummys ${PROG} prog_output
+	VAL_LOG=`ls | grep val_log | wc -l`
+	del_dummys ${VAL_LOG} val_log
+	AOUT=`ls | grep a.out | wc -l`
+	del_dummys ${AOUT} a.out
+}
 
-echo -e "\e[47;34m\e[1m"
-echo -e "\e[47;34mno_nl_main.c_test\e[0m"
-while [ $SIZE -le $COUNT ]
-do
-	echo -e "\e[32mSIZE=$SIZE\e[0m"
-	cc mains/no_nl_main.c $CFILES $FLAGS -g
-	valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-	LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-	if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-		echo  No_leaks!
-	else
-		cat log_buff_file
-	fi
-	cat cases/no_nl/* -e > origin_output
-	cat prog_output -e > prog_output_e
-	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
-	if [ ${DIFF_RESULT} -eq 0 ]; then
-		echo -e "\e[34mDiff_ok\e[0m"
-	else
-		echo -e "\e[31mDiff_KO\e[0m"
-	fi
-	rm log_buff_file prog_output origin_output prog_output_e
-	((SIZE++))
-done
-SIZE=1
+run_ivld_fd() {
+	SIZE=1
+	echo -e "\e[47;1;34mtest_file: $1\e[0m" #gen while line
+	echo "==============================test_file: $1==============================" >> trace
+	echo >> trace
 
-echo -e "\e[47;34m\e[1m"
-echo -e "\e[47;34mnl_btw_str_main.c_test\e[0m"
-while [ $SIZE -le $COUNT ]
-do
-	echo -e "\e[32mSIZE=$SIZE\e[0m"
-	cc mains/nl_btw_str_main.c $CFILES $FLAGS -g
-	valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-	LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-	if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-		echo  No_leaks!
-	else
-		cat log_buff_file
-	fi
-	cat cases/nl_btw_str/* -e > origin_output
-	cat prog_output -e > prog_output_e
-	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
-	if [ ${DIFF_RESULT} -eq 0 ]; then
-		echo -e "\e[34mDiff_ok\e[0m"
-	else
-		echo -e "\e[31mDiff_KO\e[0m"
-	fi
-	rm log_buff_file prog_output origin_output prog_output_e
-	((SIZE++))
-done
-SIZE=1
+	while [ $SIZE -le $COUNT ]
+	do
+		echo -en "\e[1;32mbuff=$SIZE \e[0m"
+		echo "[buff=$SIZE]" >> trace
 
-echo -e "\e[47;34m\e[1m"
-echo -e "\e[47;34mothers_main.c_test\e[0m"
-while [ $SIZE -le 5 ]
-do
-	echo -e "\e[32mSIZE=$SIZE\e[0m"
-	cc mains/others_main.c $CFILES $FLAGS -g
-	valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-	LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-	if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-		echo  No_leaks!
-	else
-		cat log_buff_file
-	fi
-	cat cases/others/* -e > origin_output
-	cat prog_output -e > prog_output_e
-	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
-	if [ ${DIFF_RESULT} -eq 0 ]; then
-		echo -e "\e[34mDiff_ok\e[0m"
-	else
-		echo -e "\e[31mDiff_KO\e[0m"
-	fi
-	rm log_buff_file prog_output origin_output prog_output_e
-	((SIZE++))
-done
-SIZE=1
+		cc $1 $BF$SIZE $CFILES $FLAGS
+		valgrind $VLGND_FLG ./a.out | cat -e > prog_output
+		echo -n "(null)(null)" > origin_output
+		DIFF_RESULT=`diff origin_output prog_output | wc -l`
+		if [ ${DIFF_RESULT} -eq 0 ]; then
+			echo -n diff_ok!
+		else
+			echo -en "\e[31mDIFF_KO\e[0m"
 
-echo -e "\e[47;34m\e[1m"
-echo -e "\e[47;34minvaild_fd.c_test\e[0m"
-while [ $SIZE -le 5 ]
-do
-	echo -e "\e[32mSIZE=$SIZE\e[0m"
-	cc mains/ivld_fd.c $CFILES $FLAGS -g
-	valgrind --log-file=log_buff_file --tool=memcheck --leak-check=full ./a.out > prog_output
-	LEAK_RESULT=`cat log_buff_file | grep "ERROR" | cut -f 5 -d'='`
-	if [ "${LEAK_RESULT}" = "${LEAK}" ];then
-		echo  No_leaks!
-	else
-		cat log_buff_file
-	fi
-	echo -n "(null)(null)" > origin_output
-	cat prog_output -e > prog_output_e
-	DIFF_RESULT=`diff origin_output prog_output_e | wc -l`
-	if [ ${DIFF_RESULT} -eq 0 ]; then
-		echo -e "\e[34mDiff_ok\e[0m"
-	else
-		echo -e "\e[31mDiff_KO\e[0m"
-	fi
-	rm log_buff_file prog_output origin_output prog_output_e
-	((SIZE++))
-done
-SIZE=1
-rm a.out
+			echo "============diff_ko============" >> trace
+
+			echo "origin_output == $1" >> trace
+			cat origin_output >> trace
+			echo >> trace
+			echo "your_output" >> trace
+			cat prog_output >> trace
+			echo
+			echo "===============================" >> trace
+			echo >> trace
+		fi
+
+		echo -n " / "
+
+		LEAK_RESULT=`cat val_log | grep "ERROR" | cut -f 5 -d'='`
+		if [ "${LEAK_RESULT}" = "${NO_LEAK}" ]; then
+			echo -n No_leaks!
+		else
+#			cat val_log
+			echo -en "\e[31mLEAK_KO\e[0m"
+			cat	val_log >> trace
+			echo >> trace
+		fi
+
+		((SIZE++))
+		echo
+	done
+	echo
+}
+
+run_test() {
+	SIZE=1
+	echo -e "\e[47;1;34mtest_file: $1\e[0m" #gen while line
+	echo "==============================test_file: $1==============================" >> trace
+	echo >> trace
+
+	while [ $SIZE -le $COUNT ]
+	do
+		echo -en "\e[1;32mbuff=$SIZE \e[0m"
+		echo "[buff=$SIZE]" >> trace
+
+		cc $1 $BF$SIZE $CFILES $FLAGS
+		valgrind $VLGND_FLG ./a.out | cat -e > prog_output
+		cat -e $2* > origin_output
+		DIFF_RESULT=`diff origin_output prog_output | wc -l`
+		if [ ${DIFF_RESULT} -eq 0 ]; then
+			echo -n diff_ok!
+		else
+			echo -en "\e[31mDIFF_KO\e[0m"
+
+			echo "============diff_ko============" >> trace
+
+			echo "origin_output == $1" >> trace
+			cat origin_output >> trace
+			echo >> trace
+			echo "your_output" >> trace
+			cat prog_output >> trace
+			echo "===============================" >> trace
+			echo >> trace
+		fi
+
+		echo -n " / "
+
+		LEAK_RESULT=`cat val_log | grep "ERROR" | cut -f 5 -d'='`
+		if [ "${LEAK_RESULT}" = "${NO_LEAK}" ]; then
+			echo -n No_leaks!
+		else
+#			cat val_log
+			echo -en "\e[31mLEAK_KO\e[0m"
+			cat	val_log >> trace
+			echo >> trace
+		fi
+
+		((SIZE++))
+		echo
+	done
+	echo
+}
+
+#start main
+	clear
+	echo "launching hoy_tester"
+	echo -e "\e[1;32m.__                       __                   __                \e[0m"
+	echo -e "\e[1;32m|  |__   ____ ___.__.   _/  |_  ____   _______/  |_  ___________ \e[0m"
+	echo -e "\e[1;32m|  |  \ /  _ <   |  |   \   __\/ __ \ /  ___/\   __\/ __ \_  __ \ \e[0m"
+	echo -e "\e[1;32m|   Y  (  <_> )___  |    |  | \  ___/ \___ \  |  | \  ___/|  | \/ \e[0m"
+	echo -e "\e[1;32m|___|  /\____// ____|____|__|  \___  >____  > |__|  \___  >__|   \e[0m"
+	echo -e "\e[1;32m     \/       \/   /_____/         \/     \/            \/       \e[0m"
+	echo
+
+	TRACE=`ls | grep trace | wc -l`
+	del_dummys ${TRACE} trace
+	tester_init
+	run_test mains/nl_main.c cases/nl/
+	run_test mains/no_nl_main.c cases/no_nl/
+	run_test mains/nl_btw_str_main.c cases/nl_btw_str/
+	run_test mains/others_main.c cases/others/
+	run_ivld_fd mains/ivld_fd.c
+	tester_init
+	echo "check your trace file to see more information."
+#end main
