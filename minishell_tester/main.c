29963adbda43fcdc87f369e63ba4080601f39f93
Hoyoung Song <hoysong@c2r5s4.42gyeongsan.kr>
2025-04-19 22:47:27 +0900

    read함수 내에서 무한루프가 걸리지 않았나 싶다. 해결하였음.

--- minishell_tester/main.c
+++ minishell_tester/main.c
@@ -6,7 +6,7 @@
 
 extern int	find_minishell( void );
 
-t_info	init_info(char **argv, char **env)
+static t_info	init_info(char **argv, char **env)
 {
 	t_info	info;
 
@@ -33,55 +33,47 @@ char	*test_1[] = {
 	NULL
 };
 
-void	run_tester(t_info *info)
+static void	run_tester(t_info *info)
 {
 	pid_t	pid;
 	int		to_mini_fd[2];
 	int		from_mini_fd[2];
-	int		err_from_mini_fd[2];
 	char	outputs[1024];
 
 	pipe(to_mini_fd);
 	pipe(from_mini_fd);
-	pipe(err_from_mini_fd);
 	pid = fork();
 	if (pid) // 부모
 	{
 		sleep(1);
 		write(to_mini_fd[1], "ls\n", 3);
-		sleep(3);
+		sleep(1);
 		write(to_mini_fd[1], "echo hi\n", 8);
-		sleep(3);
+		sleep(1);
 		write(to_mini_fd[1], "exit\n", 5);
-		sleep(3);
+		sleep(1);
 
 		//stdin
 		waitpid(pid, NULL, 0);
-		int len = read(from_mini_fd[0], outputs, sizeof(outputs) - 1);
-		outputs[len] = '\0';
-		put_str(RED, "now_parent\n");
-		printf("input: %d\n", len);
-		printf("%s\n", outputs);
-
-		//err
-		len = read(err_from_mini_fd[0], outputs, sizeof(outputs) - 1);
-		outputs[len] = '\0';
-		put_str(RED, "now_parent\n");
-		printf("input: %d\n", len);
-		printf("%s\n", outputs);
-
+		int len;
+		while (1)
+		{
+			len = read(from_mini_fd[0], outputs, sizeof(outputs) - 1);
+			outputs[len] = '\0';
+			printf("input_len: %d\n", len);
+			printf("%s\n", outputs);
+			if (len < 1023)
+				break ;
+		}
 	}
 	else // 자식
 	{
 		dup2(to_mini_fd[0], 0);
 		dup2(from_mini_fd[1], 1);
-		dup2(err_from_mini_fd[1], 2);
 		close(to_mini_fd[0]);
 		close(to_mini_fd[1]);
 		close(from_mini_fd[0]);
 		close(from_mini_fd[1]);
-		close(err_from_mini_fd[0]);
-		close(err_from_mini_fd[1]);
 		printf("%d\n", execve("/usr/bin/valgrind", test_1, info->env));
 	}
 	return ;
